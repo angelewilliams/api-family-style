f1c8756199ec97a078fb9f4d2c9fb6d38462b7c4
Angele Williams <angele.williams5@gmail.com>
2022-06-08 23:11:32 -0600

    Add basic structure and starting data for recipes

--- data.js
+++ data.js
@@ -3,19 +3,58 @@ const recipes = [
       id: 1, 
       title: 'Angel Food Cake', 
       url: 'https://www.joyofbaking.com/AngelFoodCake.html', 
-      description: 'this recipe is different from the OG',
+      notes: 'this recipe is different from the OG',
       submittedBy: 'Angele',
-      tags: ['dessert', 'birthday']
+      group: 'williams',
+      tags: ['dessert']
     },
     { 
         id: 2, 
         title: 'Zucchini Pancakes', 
-        url: 'https://cooking.nytimes.com/recipes/1013960-zucchini-pancakes?action=click&module=RecipeBox&pgType=recipebox-page&region=all&rank=41', 
-        description: 'Tasty and good for using up zucchini',
-        otherContext: 'Best eaten fresh',
+        url: 'https://www.foodnetwork.com/recipes/ina-garten/zucchini-pancakes-recipe-1948910', 
+        notes: 'Tasty and good for using up zucchini',
         submittedBy: 'Angele',
-        tags: ['dinner']
+        group: 'williams',
+        tags: ['main']
     },
+    { 
+        id: 3, 
+        title: 'Rhubarb Cake', 
+        url: 'https://docs.google.com/document/d/1oAojta_-D-KpJnDwPOP3zbnu4fisqBhYr9FJGKgOijQ/edit?usp=sharing', 
+        notes: 'exceptional with a cup of coffee',
+        submittedBy: 'Angele',
+        group: 'williams',
+        tags: ['dessert']
+    },
+    { 
+        id: 4, 
+        title: 'Galette', 
+        url: 'https://docs.google.com/document/d/1zc2Vb_TjfBzFTvFsSbgNkc1nUxStkMIbUabd9ICD6UA/edit?usp=sharing', 
+        notes: 'I would recommend using apples, pear, or any kind of stone fruit',
+        submittedBy: 'Angele',
+        group: 'williams',
+        tags: ['dessert']
+    },
+    { 
+        id: 5, 
+        title: 'Green Sauce',
+        url: 'https://www.foodnetwork.com/recipes/good-on-everything-green-sauce-12241248', 
+        notes: 'Tasty and good for using up zucchini',
+        submittedBy: 'Angele',
+        group: 'williams',
+        tags: ['sauce']
+    },
+    { 
+        id: 6, 
+        title: 'Butternut Squash Kale Salad',
+        url: 'https://docs.google.com/document/d/17SKnyCb9ypZUV3XfdXIEwgL6hyNLOJ36PGWYtpC6Kt4/edit?usp=sharing', 
+        notes: 'Tasty and good for using up zucchini',
+        submittedBy: 'Angele',
+        group: 'williams',
+        tags: ['salad']
+    },
+    
+
 ]
 
 module.exports = recipes;
