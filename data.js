73603ed575337caa8541c7c958b2bd73e0eab886
Angele Williams <angele.williams5@gmail.com>
2022-06-12 18:26:58 -0600

    Add Procfile

--- data.js
+++ data.js
@@ -8,7 +8,7 @@ const recipes = [
       group: 'group1',
       tag: 'dessert'
     },
-    { 
+    {
         id: 2, 
         title: 'Zucchini Pancakes', 
         url: 'https://www.foodnetwork.com/recipes/ina-garten/zucchini-pancakes-recipe-1948910', 
@@ -16,6 +16,34 @@ const recipes = [
         submittedBy: 'Angele',
         group: 'group1',
         tag: 'main'
+    },
+    
+    { 
+        id: 5, 
+        title: 'Green Sauce',
+        url: 'https://www.foodnetwork.com/recipes/good-on-everything-green-sauce-12241248', 
+        notes: 'Tasty and good for using up zucchini',
+        submittedBy: 'Angele',
+        group: 'group1',
+        tag: 'sauce'
+    },
+    { 
+        id: 6, 
+        title: 'Butternut Squash Kale Salad',
+        url: 'https://docs.google.com/document/d/17SKnyCb9ypZUV3XfdXIEwgL6hyNLOJ36PGWYtpC6Kt4/edit?usp=sharing', 
+        notes: 'Great to meal prep and make for a quick lunch',
+        submittedBy: 'Angele',
+        group: 'group1',
+        tag: 'salad'
+    },
+    { 
+        id: 7, 
+        title: 'Hummus With Sizzled Dolmades',
+        url: 'https://www.bonappetit.com/recipe/green-hummus-with-sizzled-dolmades', 
+        notes: "haven't tried yet, but looks delicious!",
+        submittedBy: 'Angele',
+        group: 'group1',
+        tag: 'snack'
     },
     { 
         id: 3, 
@@ -34,24 +62,6 @@ const recipes = [
         submittedBy: 'Angele',
         group: 'group1',
         tag: 'dessert'
-    },
-    { 
-        id: 5, 
-        title: 'Green Sauce',
-        url: 'https://www.foodnetwork.com/recipes/good-on-everything-green-sauce-12241248', 
-        notes: 'Tasty and good for using up zucchini',
-        submittedBy: 'Angele',
-        group: 'group1',
-        tag: 'sauce'
-    },
-    { 
-        id: 6, 
-        title: 'Butternut Squash Kale Salad',
-        url: 'https://docs.google.com/document/d/17SKnyCb9ypZUV3XfdXIEwgL6hyNLOJ36PGWYtpC6Kt4/edit?usp=sharing', 
-        notes: 'Tasty and good for using up zucchini',
-        submittedBy: 'Angele',
-        group: 'group1',
-        tag: 'salad'
     },
     
 
